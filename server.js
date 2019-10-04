97217edb4c07594789bce7d71dcb7222aab73664
pushpendrakr <52634462+pushpendrakr@users.noreply.github.com>
2019-10-04 12:50:54 +0530

    hostedauth

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use('/views/add_product.html',(req,res)=>{
 })
 app.get('/auth/facebook', passport.authenticate('facebook',{scope:['email']}));
 app.get('/auth/facebook/callback',passport.authenticate('facebook', {   
-                                        successRedirect: '/',
+                                        successRedirect: '/views/client',
                                         failureRedirect: '/' }));
 app.listen(process.env.PORT||1234,()=>{console.log('server started ')})
 
